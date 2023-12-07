13a977ea18171841abadbb9e5c4bb2e65df9a231
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-12-07 20:15:42 -0300

    feat: Inclusão classes de podcast e episodio

---
+++ Screen Sound/Models/Episodio.cs
@@ -0,0 +1,28 @@
+﻿class Episodio
+{    
+    private readonly List<string> convidados = new();
+
+    public int Ordem { get; set; }
+    public string Titulo { get; set; }
+    public int Duracao { get; set; }
+    public string DescricaoResumida => $"{Ordem}. Título: {Titulo}, Duração: {Duracao} min";
+
+    public Episodio(string titulo, int duracao)
+    {
+        this.Titulo = titulo;
+        this.Duracao = duracao;
+    }
+
+    public void AdicionarConvidado(string convidado)
+    {
+        convidados.Add(convidado);
+    }
+
+    public void ExibirDetalhes()
+    {
+        ScreenSoundUI.EscreverFormatado($"\t\tOrdem: {Ordem}.");
+        ScreenSoundUI.EscreverFormatado($"\t\tTitulo: {Titulo}.");
+        ScreenSoundUI.EscreverFormatado($"\t\tDuracao: {Duracao} min.");
+        ScreenSoundUI.EscreverFormatado($"\t\tConvidados: {string.Join(", ", convidados)}.");
+    }
+}
---
+++ Screen Sound/Models/Podcast.cs
@@ -0,0 +1,34 @@
+﻿class Podcast
+{
+    private List<Episodio> episodios = new List<Episodio>();
+
+    public string Host { get; set; }
+    public string Nome { get; set; }
+    public int TotalEpisodios => episodios.Count;
+    public string DescricaoResumida => $"Nome: {Nome}, Host: {Host}";
+
+    public Podcast(string host, string nome)
+    {
+        this.Host = host;
+        this.Nome = nome;
+    }
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodio.Ordem = episodios.Count;
+        episodios.Add(episodio);
+    }
+
+    public void ExibirEpisodiosDoPodcast()
+    {
+        ScreenSoundUI.EscreverFormatado($"\tLista de episódios do podcast '{Nome}'.\n");
+
+        foreach (var episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            episodio.ExibirDetalhes();
+            Console.WriteLine();
+        }
+
+        ScreenSoundUI.EscreverFormatado($"\tO Host desse podcast é {Host}, com um total de {TotalEpisodios} episódios.");
+    }
+}
