6a9627dd8aa84e3287304397e65b56dc09f6043e
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-12-07 16:43:04 -0300

    feat: Criação classe Album

---
+++ Screen Sound/Album.cs
@@ -0,0 +1,25 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}\n");
+
+        foreach (var musica in musicas)
+        {
+            musica.ExibirFichaTecnica();
+        }
+
+        Console.WriteLine($"A duração desse álbum é de {DuracaoTotal} segundos");
+    }
+}
